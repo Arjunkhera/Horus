874d8dea9ccc0f55943edae408aed897cf799be3
Arjun Khera <arjunkherais@gmail.com>
2026-05-09 16:00:27 +0530

    feat(reader): page actions icon row replaces overflow menu

--- packages/reader/icons.jsx
+++ packages/reader/icons.jsx
@@ -29,6 +29,12 @@ window.Icon = {
       ? <svg viewBox="0 0 24 24" fill="currentColor" stroke="currentColor"><path d="M16 3l5 5-3 1-4 4-1 5-2-2-4 4-1-1 4-4-2-2 5-1 4-4z"/></svg>
       : <svg viewBox="0 0 24 24"><path d="M16 3l5 5-3 1-4 4-1 5-2-2-4 4-1-1 4-4-2-2 5-1 4-4z"/></svg>
   ),
+  Pencil: () => (
+    <svg viewBox="0 0 24 24"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
+  ),
+  Trash: () => (
+    <svg viewBox="0 0 24 24"><polyline points="3 6 5 6 21 6"/><path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"/><path d="M10 11v6M14 11v6"/><path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"/></svg>
+  ),
   Plus: () => (
     <svg viewBox="0 0 24 24"><line x1="12" y1="5" x2="12" y2="19"/><line x1="5" y1="12" x2="19" y2="12"/></svg>
   ),
